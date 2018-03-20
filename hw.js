2cd8585002a244acf98c728ab8c3c41cc43cfb7c
Joan Marie Verba <verba001@umn.edu>
2018-03-20 15:17:46 -0500

    partial

--- hw.js
+++ hw.js
@@ -1,57 +1,16 @@
       "use strict";
       
       // Initial array of items
-      var items = ["literature", "library", "books", "bookstore", "author", "fantasy"];
+      let items = ["astronomy", "space", "satellite", "astronaut", "moon", "star", "aurora", "planet"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
-      function displayGIFs() {
-
-       let item = $(this).attr("data-name");
-       console.log("data-name ", item);
-
-        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=Ebv1xRbHRFV9SafJDmTUY22Qy0LWjZzs&limit=12";
-        console.log(queryURL);
-        // Creates AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            console.log(response.data[0].rating);
-          // // Creating a div to hold the gif (automatically supplies the </div> at the end)
-          // let gifDiv = $("<div class='picture'>");
-
-          // //  Storing the rating data
-          // let gifRating = response.data[0].rating;
-          // // Creating an element to have the rating displayed
-          // let aboveGIF = $("<p>").text(`Rating: ${gifRating}`);
-          // // Displaying the rating
-          // gifDiv.append(aboveGIF);
-
-          // // Retrieving the URL for the gif
-          // let imgURL = response.data[0].bitly_gif.url;
-          // // Creating an element to hold the image
-          // let image = $("<img>").attr("src", imgURL);
-          // // Appending the image
-          // gifDiv.append(image);
-
-          // //clear previous input 
-          // $("#thegifs").empty();
-
-          // // Putting the gif on the page
-          // $("#thegifs").append(gifDiv);
-        });
-
-      }
-
-      // Function for displaying movie data
+      // Function for placing buttons on screen
       function renderButtons() {
 
-        // Deletes the movies prior to adding new movies
+        // Deletes the on screen data prior to adding new items
         // (this is necessary otherwise you will have repeat buttons)
         $("#gifbuttons").empty();
 
-        // Loops through the array of movies
+        // Loops through the array of items
         for (var i = 0; i < items.length; i++) {
 
           // Then dynamicaly generates buttons for each movie in the array
@@ -67,6 +26,62 @@
           $("#gifbuttons").append(a);
         }
       }
+      renderButtons();
+
+      //when one of the top buttons are clicked
+      // $(".topbuttons").click(function () {
+      //   let data = $(this).attr("data-name");
+      //   console.log("top button clicked");
+      //   displayGIFs(data);
+      // })
+      $(document).on("click", ".topbuttons", displayGIFs);
+
+
+      // displayGIFs function re-renders the HTML to display the appropriate content
+      function displayGIFs() {
+
+       let item = $(this).attr("data-name");
+       console.log("data-name ", item);
+
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=Ebv1xRbHRFV9SafJDmTUY22Qy0LWjZzs&limit=12";
+        console.log(queryURL);
+        // Creates AJAX call for the specific button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            console.log(response.data[0].rating);
+          // Creating a div to hold the gif (automatically supplies the </div> at the end)
+          let gifDiv = $("<div class='picture'>");
+
+          for (var i = 0; i < 12; i++) {
+
+            //  Storing the rating data
+            let gifRating = response.data[i].rating;
+            // Creating an element to have the rating displayed
+            let aboveGIF = $("<p>").text(`Rating: ${gifRating}`);
+            // Displaying the rating
+            gifDiv.append(aboveGIF);
+
+            // Retrieving the URL for the gif
+            let imgURL = response.data[i].images.fixed_height.url;
+            // Creating an element to hold the image
+            let image = $("<img>").attr("src", imgURL);
+            // Appending the image
+            gifDiv.append(image);
+          }
+
+          //clear previous input 
+          $("#thegifs").empty();
+
+          // Putting the gif on the page
+          $("#thegifs").append(gifDiv);
+        });
+
+      }
+
+
 
       // This function handles events where the add category button is clicked
       $("#addcategory").on("click", function(event) {
@@ -76,14 +91,13 @@
 
         // The movie from the textbox is then added to our array
         items.push(iteminput);
-
+        console.log("iteminput ", iteminput);
         // Calling renderButtons which handles the processing of our movie array
         renderButtons();
-
       });
 
-      // Adding click event listeners to all elements with a class of "btnform"
-      $(document).on("click", "#btnform", displayGIFs);
+      // Adding click event listeners to all elements with a class of "addcategory"
+      $(document).on("click", "#addcategory", displayGIFs);
 
       // Calling the renderButtons function to display the intial buttons
       renderButtons();
